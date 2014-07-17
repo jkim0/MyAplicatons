6015a48757caf28c9b3b3101517c84b2e2a354fc
Joungchoul Kim <joungchoul.kim@windriver.com>
2014-07-17 19:00:49 +0900

    impemented GrammarProvider
    Signed-off-by: Joungchoul Kim <joungchoul.kim@windriver.com>

--- GrammarBook/src/com/loyid/grammarbook/GrammarProvider.java
+++ GrammarBook/src/com/loyid/grammarbook/GrammarProvider.java
@@ -1,8 +1,11 @@
 package com.loyid.grammarbook;
 
+import java.util.HashMap;
+
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -24,6 +27,52 @@ public class GrammarProvider extends ContentProvider {
 
     private DatabaseHelper mDatabaseHelper = null;
     
+    /**
+     * A projection map used to select columns from the database
+     */
+    private static HashMap<String, String> sGrammarsProjectionMap;
+    
+    private static final int GRAMMARS = 0;
+    private static final int GRAMMAR_ID = 1;
+    private static final int MEANING = 2;
+    private static final int MEANING_ID = 3;
+    private static final int MAPPING = 4;
+    private static final int MAPPING_ID = 5;
+    
+    /**
+     * A UriMatcher instance
+     */
+    private static final UriMatcher sUriMatcher;
+    
+    static {
+    	
+    	sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    	sUriMatcher.addURI(GrammarProviderContract.AUTHORITY,
+    			GrammarProviderContract.Grammars.TABLE_NAME, GRAMMARS);
+    	sUriMatcher.addURI(GrammarProviderContract.AUTHORITY,
+    			GrammarProviderContract.Grammars.TABLE_NAME + "/#", GRAMMARS);
+    	sUriMatcher.addURI(GrammarProviderContract.AUTHORITY,
+    			GrammarProviderContract.Meanings.TABLE_NAME, MEANING);
+    	sUriMatcher.addURI(GrammarProviderContract.AUTHORITY,
+    			GrammarProviderContract.Meanings.TABLE_NAME + "/#", MEANING);
+    	sUriMatcher.addURI(GrammarProviderContract.AUTHORITY,
+    			GrammarProviderContract.Mappings.TABLE_NAME, MAPPING);
+    	sUriMatcher.addURI(GrammarProviderContract.AUTHORITY,
+    			GrammarProviderContract.Mappings.TABLE_NAME + "/#", MAPPING);
+    	
+    	sGrammarsProjectionMap = new HashMap<String, String>();
+    	sGrammarsProjectionMap.put(GrammarProviderContract.Grammars._ID,
+    			GrammarProviderContract.Grammars._ID);
+    	sGrammarsProjectionMap.put(GrammarProviderContract.Grammars.COLUMN_NAME_GRAMMAR,
+    			GrammarProviderContract.Grammars.COLUMN_NAME_GRAMMAR);
+    	sGrammarsProjectionMap.put(GrammarProviderContract.Grammars.COLUMN_NAME_MEANING,
+    			GrammarProviderContract.Grammars.COLUMN_NAME_MEANING);
+    	sGrammarsProjectionMap.put(GrammarProviderContract.Grammars.COLUMN_NAME_CREATED_DATE,
+    			GrammarProviderContract.Grammars.COLUMN_NAME_CREATED_DATE);
+    	sGrammarsProjectionMap.put(GrammarProviderContract.Grammars.COLUMN_NAME_MODIFIED_DATE,
+    			GrammarProviderContract.Grammars.COLUMN_NAME_MODIFIED_DATE);
+    }
+    
 	public GrammarProvider() {
 	}
 
@@ -39,10 +88,23 @@ public class GrammarProvider extends ContentProvider {
 			db.execSQL("CREATE TABLE " + GrammarProviderContract.Grammars.TABLE_NAME + " ("
 					+ GrammarProviderContract.Grammars._ID + " INTEGER PRIMARY KEY,"
 					+ GrammarProviderContract.Grammars.COLUMN_NAME_GRAMMAR + " TEXT,"
-					+ GrammarProviderContract.Grammars.COLUMN_NAME_TYPE + " INTEGER,"
 					+ GrammarProviderContract.Grammars.COLUMN_NAME_MEANING + " TEXT,"
 					+ GrammarProviderContract.Grammars.COLUMN_NAME_CREATED_DATE + " INTEGER,"
 					+ GrammarProviderContract.Grammars.COLUMN_NAME_MODIFIED_DATE + " INTEGER"
+					+ ");");
+			
+			db.execSQL("CREATE TABLE " + GrammarProviderContract.Meanings.TABLE_NAME + " ("
+					+ GrammarProviderContract.Meanings._ID + " INTEGER PRIMARY KEY,"
+					+ GrammarProviderContract.Meanings.COLUMN_NAME_WORD + " TEXT,"
+					+ GrammarProviderContract.Meanings.COLUMN_NAME_TYPE + " TEXT,"
+					+ GrammarProviderContract.Meanings.COLUMN_NAME_CREATED_DATE + " INTEGER,"
+					+ GrammarProviderContract.Meanings.COLUMN_NAME_MODIFIED_DATE + " INTEGER"
+					+ ");");
+			
+			db.execSQL("CREATE TABLE " + GrammarProviderContract.Meanings.TABLE_NAME + " ("
+					+ GrammarProviderContract.Mappings._ID + " INTEGER PRIMARY KEY,"
+					+ GrammarProviderContract.Mappings.COLUMN_NAME_GRAMMAR_ID + " INTEGER,"
+					+ GrammarProviderContract.Mappings.COLUMN_NAME_MEANING_ID + " INTEGER"
 					+ ");");
 		}
 
@@ -54,7 +116,9 @@ public class GrammarProvider extends ContentProvider {
 					+ newVersion + ", which will destroy all old data");
 			
 			// Kills the table and existing data
-			db.execSQL("DROP TABLE IF EXISTS grammers");
+			db.execSQL("DROP TABLE IF EXISTS " + GrammarProviderContract.Grammars.TABLE_NAME);
+			db.execSQL("DROP TABLE IF EXISTS " + GrammarProviderContract.Meanings.TABLE_NAME);
+			db.execSQL("DROP TABLE IF EXISTS " + GrammarProviderContract.Mappings.TABLE_NAME);
 			
 			// Recreates the database with a new version
 			onCreate(db);
--- GrammarBook/src/com/loyid/grammarbook/GrammarProviderContract.java
+++ GrammarBook/src/com/loyid/grammarbook/GrammarProviderContract.java
@@ -9,11 +9,132 @@ public final class GrammarProviderContract {
 	private GrammarProviderContract() {
 	}
 	
+	public static final class Meanings implements BaseColumns {
+		public static final String TABLE_NAME = "meanings";
+		
+		/*
+		 * URI definitions
+		 */
+		
+		/**
+		 * The table name offered by this provider
+		 */
+		private static final String SCHEME = "content://";
+		
+		/*
+		 * Path parts for the URIs
+		 */
+		/**
+		 * Path part for the Meanings URI
+		 */
+		private static final String PATH_MEANING = "/meanings";
+		
+		/**
+		 * Path part for the Meaning ID URI
+		 */
+		private static final String PATH_MEANING_ID = "/meanings/";
+		
+		/**
+		 * 0-relative position of a Meaning ID segment in the path part of a Meaning ID URI
+		 */
+		public static final int MEANING_ID_PATH_POSITION = 1;
+		
+		/**
+		 * The content:// style URL for this table
+		 */
+		public static final Uri CONTENT_URI = Uri.parse(SCHEME + AUTHORITY + PATH_MEANING);
+		
+		/**
+		 * The content URI base for a single meaning. Callers must
+		 * append a numeric meaning id to this URI to retrieve a meaning
+		 */
+		public static final Uri CONTENT_MEANING_ID_URI_BASE = Uri.parse(SCHEME + AUTHORITY + PATH_MEANING_ID);
+		
+		/**
+		 * The content URI match pattern for a single meaning, specified by its ID. Use this to match
+		 * incoming URIs or to construct an Intent.
+		 */
+		public static final Uri CONTENT_MEANING_ID_URI_PATTERN = Uri.parse(SCHEME + AUTHORITY + PATH_MEANING_ID + "/#");
+		
+		public static final String COLUMN_NAME_WORD = "word";
+		
+		public static final String COLUMN_NAME_TYPE = "type";
+		
+		/**
+		 * Column name for the creation timestamp
+		 * <P>Type: INTEGER (long from System.curentTimeMillis())</P>
+		 */	
+		public static final String COLUMN_NAME_CREATED_DATE = "created";
+		
+		/**
+		 * Column name for the creation timestamp
+		 * <P>Type: INTEGER (long from System.curentTimeMillis())</P>
+		 */	
+		public static final String COLUMN_NAME_MODIFIED_DATE = "modified";
+		
+		/**
+		 * The default sort order for this table
+		 */
+		public static final String DEFAULT_SORT_ORDER = COLUMN_NAME_WORD + " ASC"; //ASC or DESC
+	}
+	
+	public static final class Mappings implements BaseColumns {
+		public static final String TABLE_NAME = "mappings";
+		
+		/*
+		 * URI definitions
+		 */
+		
+		/**
+		 * The table name offered by this provider
+		 */
+		private static final String SCHEME = "content://";
+		
+		/*
+		 * Path parts for the URIs
+		 */
+		/**
+		 * Path part for the Mappings URI
+		 */
+		private static final String PATH_MAPPING = "/mappings";
+		
+		/**
+		 * Path part for the Mapping ID URI
+		 */
+		private static final String PATH_MAPPING_ID = "/mappings/";
+		
+		/**
+		 * 0-relative position of a Mapping ID segment in the path part of a Mapping ID URI
+		 */
+		public static final int MAPPING_ID_PATH_POSITION = 1;
+		
+		/**
+		 * The content:// style URL for this table
+		 */
+		public static final Uri CONTENT_URI = Uri.parse(SCHEME + AUTHORITY + PATH_MAPPING);
+		
+		/**
+		 * The content URI base for a single mapping. Callers must
+		 * append a numeric mapping id to this URI to retrieve a mapping
+		 */
+		public static final Uri CONTENT_MAPPING_ID_URI_BASE = Uri.parse(SCHEME + AUTHORITY + PATH_MAPPING_ID);
+		
+		/**
+		 * The content URI match pattern for a single mapping, specified by its ID. Use this to match
+		 * incoming URIs or to construct an Intent.
+		 */
+		public static final Uri CONTENT_MAPPING_ID_URI_PATTERN = Uri.parse(SCHEME + AUTHORITY + PATH_MAPPING_ID + "/#");
+		
+		public static final String COLUMN_NAME_GRAMMAR_ID = "grammar_id";
+		
+		public static final String COLUMN_NAME_MEANING_ID = "meaing_id";
+	}
+	
 	public static final class Grammars implements BaseColumns {
 		/**
         * The table name offered by this provider
          */
-		public static final String TABLE_NAME = "grammars";
+		public static final String TABLE_NAME = "grammars";	
 		
 		/*
         * URI definitions
@@ -50,18 +171,13 @@ public final class GrammarProviderContract {
         * The content URI base for a single grammar. Callers must
         * append a numeric grammar id to this Uri to retrieve a grammar
          */
-		public static final Uri CONTENT_ID_URI_BASE = Uri.parse(SCHEME + AUTHORITY + PATH_GRAMMAR_ID);
+		public static final Uri CONTENT_GRAMMAR_ID_URI_BASE = Uri.parse(SCHEME + AUTHORITY + PATH_GRAMMAR_ID);
 		
 		/**
         * The content URI match pattern for a single grammar, specified by its ID. Use this to match
         * incoming URIs or to construct an Intent.
          */
-		public static final Uri CONTENT_ID_URI_PATTERN = Uri.parse(SCHEME + AUTHORITY + PATH_GRAMMAR_ID + "/#");
-		
-		/**
-        * The default sort order for this table
-         */
-		public static final String DEFAULT_SORT_ORDER = "grammar ASC"; //ASC or DESC
+		public static final Uri CONTENT_GRAMMAR_ID_URI_PATTERN = Uri.parse(SCHEME + AUTHORITY + PATH_GRAMMAR_ID + "/#");
 		
 		/*
         * Column definitions
@@ -71,12 +187,6 @@ public final class GrammarProviderContract {
         * <P>Type: TEXT</P>
          */
 		public static final String COLUMN_NAME_GRAMMAR = "grammar";
-		
-		/**
-		 * Column name for the type of the grammar
-		 * <P>Type: INTEGER</P>
-		 */	
-		public static final String COLUMN_NAME_TYPE = "type";
 		
 		/**
 		 * Column name for the meaning of the grammar
@@ -94,6 +204,11 @@ public final class GrammarProviderContract {
 		 * Column name for the creation timestamp
 		 * <P>Type: INTEGER (long from System.curentTimeMillis())</P>
 		 */	
-		public static final String COLUMN_NAME_MODIFIED_DATE = "modified";		
+		public static final String COLUMN_NAME_MODIFIED_DATE = "modified";
+		
+		/**
+        * The default sort order for this table
+         */
+		public static final String DEFAULT_SORT_ORDER = COLUMN_NAME_GRAMMAR + " ASC"; //ASC or DESC
 	}
 }
